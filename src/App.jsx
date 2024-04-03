5344a6c674d50ce3ee1a748e451dd97b9c6294cb
wajahat123 <wajahat11122@gmail.com>
2024-04-03 12:52:13 +0500

    install reactjs and tailwind and make first template

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <nav className='bg-slate-600 font-bold text-gray-50 flex items-center justify-center p-4 m-4'>
+
+       
+       
+        
+        <div className='bg-black w-full h-screen text-center text-white font-bold m-4'>
+
+         
+        </div>
+       
+       
+       
+       
+
+        
+      </nav>
+    </>
+  )
+}
+
+export default App
